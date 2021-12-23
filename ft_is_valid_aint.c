44c67e071c5a306433eebb7e7f21e648223277bc
aparolar <aparolar@student.42madrid.com>
2021-12-23 21:46:51 +0100

    Update jue 23 dic 2021 21:46:51 CET

--- ft_is_valid_aint.c
+++ ft_is_valid_aint.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 11:53:45 by aparolar          #+#    #+#             */
-/*   Updated: 2021/12/22 12:56:53 by aparolar         ###   ########.fr       */
+/*   Updated: 2021/12/23 21:46:44 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,20 +45,20 @@ static	int	inside_limit(char *str, char *limit)
 
 int	ft_is_valid_aint(char *str)
 {
-	char	*tstr;
 	char	*cstr;
 
-	tstr = str;
+	if (!str)
+		return (0);
 	cstr = AINT_MAX;
-	if (*tstr == '+' || *tstr == '-')
+	if (*str == '+' || *str == '-')
 	{
-		if (*tstr == '-')
+		if (*str == '-')
 			cstr = &AINT_MIN[1];
-		tstr++;
+		str++;
 	}
-	if (ft_strlen(tstr) <= ft_strlen(cstr)
-		&& all_digit(tstr)
-		&& inside_limit(tstr, cstr))
+	if (ft_strlen(str) <= ft_strlen(cstr)
+		&& all_digit(str)
+		&& inside_limit(str, cstr))
 		return (1);
 	else
 		return (0);
