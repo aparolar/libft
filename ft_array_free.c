29b122f12d0e27774f4bf813b03b2e9bef79e391
aparolar <aparolar@student.42madrid.com>
2022-04-08 19:03:04 +0200

    Update vie 08 abr 2022 19:03:04 CEST

--- ft_array_free.c
+++ ft_array_free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_array_free.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aparolar <aparolar@student.42madrid.c      +#+  +:+       +#+        */
+/*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 11:38:15 by aparolar          #+#    #+#             */
-/*   Updated: 2022/01/03 11:45:35 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/04/08 18:34:12 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
