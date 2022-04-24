be106d58ac7141f82f3610dc164d25b68fb9ecd1
aparolar <aparolar@student.42madrid.com>
2022-04-24 21:44:20 +0200

    Update dom 24 abr 2022 21:44:20 CEST

--- lists/ft_lstiter_bpa_if.c
+++ lists/ft_lstiter_bpa_if.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 21:11:45 by aparolar          #+#    #+#             */
-/*   Updated: 2022/04/24 21:32:27 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/04/24 21:44:03 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_lstiter_bpa_if(t_list *lst, int (*f)(void *, void *, void *))
 		while (lst)
 		{
 			after = lst->next->content;
-			if (!f(before->content, lst->content, after->content))
+			if (f(before->content, lst->content, after->content))
 				return (1);
 			before = lst;
 			lst = lst->next;
