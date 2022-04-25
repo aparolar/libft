cd3a83a155987b0893fa56ed9f458eafab6dd084
aparolar <aparolar@student.42madrid.com>
2022-04-25 12:25:42 +0200

    Update lun 25 abr 2022 12:25:42 CEST

--- lists/ft_lstiter_bpa_if.c
+++ lists/ft_lstiter_bpa_if.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 21:11:45 by aparolar          #+#    #+#             */
-/*   Updated: 2022/04/25 12:03:54 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/04/25 12:25:35 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,23 @@ int	ft_lstiter_bpa_if(t_list *lst, int (*f)(void *, void *, void *))
 	t_list	*before;
 	t_list	*after;
 
-	if (lst && f)
+	if (!lst || !f)
+		return (0);
+	before = NULL;
+	while (lst)
 	{
-		before = NULL;
-		while (lst)
-		{
-			after = lst->next->content;
-			if (before && after
-				&& f(before->content, lst->content, after->content))
-				return (1);
-			else if (!before && !after && f(NULL, lst->content, NULL))
-				return (1);
-			else if (!before && f(NULL, lst->content, after->content))
-				return (1);
-			else if (!after && f(before->content, lst->content, NULL))
-				return (1);
-			before = lst;
-			lst = lst->next;
-			after = lst->next;
-		}
+		after = lst->next;
+		if (before && after
+			&& f(before->content, lst->content, after->content))
+			return (0);
+		else if (!before && !after && f(NULL, lst->content, NULL))
+			return (0);
+		else if (!before && f(NULL, lst->content, after->content))
+			return (0);
+		else if (!after && f(before->content, lst->content, NULL))
+			return (0);
+		before = lst;
+		lst = lst->next;
 	}
-	return (0);
+	return (1);
 }
