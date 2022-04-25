a8d015962845e0d274afee93ac832b0aae382495
aparolar <aparolar@student.42madrid.com>
2022-04-25 11:53:53 +0200

    Update lun 25 abr 2022 11:53:53 CEST

--- lists/ft_lstiter_count.c
+++ lists/ft_lstiter_count.c
@@ -6,7 +6,7 @@
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 10:35:45 by aparolar          #+#    #+#             */
-/*   Updated: 2022/04/25 10:38:03 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/04/25 11:53:43 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,5 @@ int	ft_lstiter_count(t_list *lst, int (*f)(void *))
 			lst = lst->next;
 		}
 	}
+	return (ret);
 }
