ad15e374132b05846279bc6cf38639e35d9497e7
aparolar <aparolar@student.42madrid.com>
2022-04-25 10:12:28 +0200

    Update lun 25 abr 2022 10:12:28 CEST

--- lists/ft_lstlast.c
+++ lists/ft_lstlast.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aparolar <aparolar@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 11:40:41 by aparolar          #+#    #+#             */
-/*   Updated: 2021/04/20 11:43:26 by aparolar         ###   ########.fr       */
+/*   Updated: 2022/04/25 10:12:09 by aparolar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*node;
-
-	node = NULL;
 	if (lst)
 	{
-		node = lst;
-		while (node->next)
-			node = node->next;
+		while (lst && lst->next)
+			lst = lst->next;
 	}
-	return (node);
+	return (lst);
 }
